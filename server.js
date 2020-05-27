3f2853ad632bde56ffd4a17794f1afb94acdff0c
Zack Awari <zack.awari16@gmail.com>
2020-05-26 21:27:13 -0400

    error changes

--- server.js
+++
@@ -1,9 +0,0 @@
-let http = require('http');
-
-function onRequest(request, response) {
-    response.writeHead(200,{'Content-Type': 'text/plain'});
-    response.write("This is the Family Dex!");
-    response.end();
-}
-
-http.createServer(onRequest).listen(3000);
